bcf13933125940adcbb820d12aec341ca4e51ec1
xzy-fly <2621745445@qq.com>
2022-02-09 17:06:22 +0800

    部分修改

--- src/main/java/com/xzy/springtest/SpringtestApplication.java
+++ src/main/java/com/xzy/springtest/SpringtestApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.xzy.springtest.mapper")
+//@MapperScan("com.xzy.springtest.mapper")
 public class SpringtestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringtestApplication.class, args);
-    }
+        System.out.print("输出内容，输出内容内容");
+        SpringApplication.run(SpringtestApplication.class, args);    }
 
 }
--- src/main/java/com/xzy/springtest/controller/HelloController.java
+++ src/main/java/com/xzy/springtest/controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.xzy.springtest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HelloController {
+    //部分修改，但还是不行
+    @RequestMapping("/index")
+    public String sayHello(){
+        return "index";
+    }
 }
