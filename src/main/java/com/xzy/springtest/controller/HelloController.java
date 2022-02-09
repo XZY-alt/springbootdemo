a4fc1ee91c261df1316b39b15e8d5b01220ca2e3
xzy-fly <2621745445@qq.com>
2022-02-09 17:32:21 +0800

    发现了请问空白的问题

--- src/main/java/com/xzy/springtest/controller/HelloController.java
+++ src/main/java/com/xzy/springtest/controller/HelloController.java
@@ -2,12 +2,16 @@ package com.xzy.springtest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
     //部分修改，但还是不行
-    @RequestMapping("/index")
+    @RequestMapping("/")
     public String sayHello(){
+        //测试输出了下面的内容
+        System.out.print("类方法，执行");
+        //应该是提示的：Cannot resolve MVC view 'index'的原因
         return "index";
     }
 }
